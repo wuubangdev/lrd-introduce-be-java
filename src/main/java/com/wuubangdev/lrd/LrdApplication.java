0815321fea8714592ed6ba87854652343e95a5fe
BangLV <wuubangdev@gmail.com>
2024-06-21 13:37:29 +0700

    init project

---
+++ src/main/java/com/wuubangdev/lrd/LrdApplication.java
@@ -0,0 +1,13 @@
+package com.wuubangdev.lrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LrdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuubangdev/lrd/LrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuubangdev.lrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
